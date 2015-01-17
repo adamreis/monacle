7f60ad8dc89c7113c690aedba6e555107330675b
Brian Donghee Shin <ds2791@columbia.edu>
2015-01-17 16:56:10 -0500

    Almost working with BrowserViewController

--- Scotch/SecondViewController.swift
+++ Scotch/SecondViewController.swift
@@ -1,25 +1,46 @@
-//
-//  SecondViewController.swift
-//  Scotch
-//
-//  Created by Brian Donghee Shin on 1/17/15.
-//  Copyright (c) 2015 Brian Donghee Shin. All rights reserved.
-//
 
 import UIKit
+import MultipeerConnectivity
 
-class SecondViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-
+class SecondViewController: UIViewController, MCBrowserViewControllerDelegate {
+    let client = PeerClient()
+    var server: PeerServer?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        client.startAdvertisingPeer()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func connectButtonTouched(sender: UIButton) {
+        if server == nil {
+            server = PeerServer()
+        }
+        let browserViewController = server!.createBrowserViewController()
+        browserViewController.delegate = self
+        
+        self.presentViewController(browserViewController, animated: true, completion: nil)
+    }
+    
+    // MARK: -
+    // MARK: MCBrowserViewControllerDelegate
+    
+    // Notifies the delegate, when the user taps the done button
+    func browserViewControllerDidFinish(browserViewController: MCBrowserViewController!) {
+        browserViewController.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    // Notifies delegate that the user taps the cancel button.
+    func browserViewControllerWasCancelled(browserViewController: MCBrowserViewController!) {
+        browserViewController.dismissViewControllerAnimated(true) {
+            println("Animation canceled")
+        }
+    }
 }
 
