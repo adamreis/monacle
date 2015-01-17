7fe1d9292b9be1a2404fe321e583dd4487d0cc0f
Brian Donghee Shin <ds2791@columbia.edu>
2015-01-17 01:24:38 -0500

    Init blank project

---
+++ Scotch/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Scotch
+//
+//  Created by Brian Donghee Shin on 1/17/15.
+//  Copyright (c) 2015 Brian Donghee Shin. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ Scotch/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Scotch
+//
+//  Created by Brian Donghee Shin on 1/17/15.
+//  Copyright (c) 2015 Brian Donghee Shin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
