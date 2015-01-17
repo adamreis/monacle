4ca2a65468eaca01d8bcb558338c2b43f9ac32d5
Adam Reis <adamhreis@gmail.com>
2015-01-17 13:15:23 -0500

    picking two videos works

--- Scotch/FirstViewController.swift
+++ Scotch/FirstViewController.swift
@@ -3,23 +3,64 @@
 //  Scotch
 //
 //  Created by Brian Donghee Shin on 1/17/15.
-//  Copyright (c) 2015 Brian Donghee Shin. All rights reserved.
+//  Copyright (c) 2015 Adam Reis. All rights reserved.
 //
 
 import UIKit
+import MobileCoreServices
+import MediaPlayer
+import AVFoundation
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
+    private var firstVideoURL: NSURL?
+    private var secondVideoURL: NSURL?
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        firstVideoURL = nil
+        secondVideoURL = nil
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        if firstVideoURL != nil && secondVideoURL != nil {
+            println("Have both videos!")
+            println(firstVideoURL)
+            println(secondVideoURL)
+        }
+    }
+    
+    @IBAction func buttonPressed(sender: AnyObject) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary){
+            
+            var imag = UIImagePickerController()
+            imag.delegate = self
+            imag.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+            imag.mediaTypes = [kUTTypeMovie]
+            imag.allowsEditing = false
+            
+            self.presentViewController(imag, animated: true, completion: nil)
+        }
+    }
+    
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        println("I've got some media")
+        let movieURL = info[UIImagePickerControllerMediaURL] as NSURL
+        if firstVideoURL == nil {
+            firstVideoURL = movieURL
+        } else if secondVideoURL == nil {
+            secondVideoURL = movieURL
+        }
+        picker.dismissViewControllerAnimated(true, completion: nil)
+        
+    }
 
 }
-
